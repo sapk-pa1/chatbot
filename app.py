ee44503fdd244e19940d14392cf1ecbf186bc3dd
sapk-pa1 <sapkotapawan099@gmail.com>
2025-03-26 23:20:22 +0545

    migrating to the streamlit for flexibility

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
 import gradio as gr
 from chatbot.chat import ConversationalChatbot
+from pathlib import Path
+import streamlit as st 
+
+ #moving on to the streamlit 
 
 # Initialize chatbot
 bot = ConversationalChatbot()
 
-def chat_function(user_input, history) -> str:
-    
-    if history: 
-        response = bot.chat(user_input, history) 
-    else: 
-        response = bot.chat(user_input) 
-    return response
-
-
-# Gradio Chat Interface
-chat_interface = gr.ChatInterface(
-    fn=chat_function,
-    textbox=gr.Textbox(placeholder="Type your message...", label="User Input"),
-    title="XChat",
-    description="A Conversational Chatbot",
-    examples=["Hello", "How are you?", "What can you do?"],
-)
-
-# Run the Gradio app
-if __name__ == "__main__":
-    chat_interface.launch()
